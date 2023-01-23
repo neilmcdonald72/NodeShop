30fd761d9b6fa63dccf704767b4e0b081c19e27b
neilmcdonald95 <neil@savormetrics.com>
2023-01-23 15:08:44 -0400

    init new repo

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require ('path');
+const app = express();
+const errorController = require('./controllers/error');
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404Page);
+
+app.listen(3000, () => {
+  console.log(`Server running`);
+});
