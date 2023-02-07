6509721a854258313fa4db3ebfe3b25a10254b8c
neilmcdonald72 <neilmcdonald72@gmail.com>
2023-02-06 21:40:33 -0400

    adding routes

--- models/products.js
+++ models/products.js
@@ -1,16 +1,44 @@
-const products = [];
-//test
+const fs = require("fs");
+const path = require("path");
+
+const getProductsFromFile = (cb) => {
+  const p = path.join(
+    path.dirname(process.mainModule.filename),
+    "data",
+    "products.json"
+  );
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    }
+    cb(JSON.parse(fileContent));
+  });
+};
+
 module.exports = class Product {
+  constructor(title) {
+    this.title = title;
+  }
 
-    constructor(title){
-        this.title = title;
-    }
+  save() {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      "data",
+      "products.json"
+    );
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
+  }
 
-    save(){
-        products.push(this);
-    }
-
-    static fetchAll(){
-        return products;
-    }
-}
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
